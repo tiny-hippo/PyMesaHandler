2996fb11b4c45c5a67dc1d861fad4b8cc7aba999
Simon <muesim@physik.uzh.ch>
2019-01-22 12:52:29 +0100

    decoupled from py_mesa_reader

--- MesaHandler/__init__.py
+++ MesaHandler/__init__.py
@@ -2,4 +2,3 @@ from MesaHandler.MesaAccess import *
 from MesaHandler.MesaInlist import *
 from MesaHandler.MesaRunner import *
 from MesaHandler.MesaFileHandler import *
-# from MesaHandler.py_mesa_reader import *
