dae062a77ff91d551a263882da18cb8029ea7d9d
Simon <muesim@physik.uzh.ch>
2019-04-01 12:19:37 +0200

    update radii definitions

--- MesaHandler/support/constants.py
+++ MesaHandler/support/constants.py
@@ -2,8 +2,10 @@
 
 M_earth = 5.97236473e+27
 M_jup = 1.89818717e+30
-M_sun = 1.98847542e+33
+M_sun = 1.9892e+33
 R_earth = 6.3781e+08
-R_jup = 7.1492e+09
-R_sun = 6.957e+10
+R_jup_eq = 7.1492e+09
+R_jup_pol = 6.6854e+09
+R_jup = 0.5 * (R_jup_eq + R_jup_pol)
+R_sun = 6.9598e+10
 L_sun = 3.828e+33
