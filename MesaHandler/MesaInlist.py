cca711fa4b0b22011a5a106f8d5ca3d306774f76
Simon <muesim@physik.uzh.ch>
2019-06-28 14:26:15 +0200

    remove @staticmethod

--- MesaHandler/MesaInlist.py
+++ MesaHandler/MesaInlist.py
@@ -40,7 +40,6 @@ class MesaInlist:
         """ Finalizes the editing process by replacing the original file """
         os.replace('inlist', self.inlist_name)
 
-    @staticmethod
     def get_X(self, Z):
         """ Calculates the hydrogen fraction given
             a heavy-element fraction Z and assuming protosolar
@@ -60,7 +59,6 @@ class MesaInlist:
         X = (1 - Z) / (1 + eta)
         return X
 
-    @staticmethod
     def get_Y(self, Z):
         """ Calculates the helium fraction given
             a heavy-element fraction Z and assuming protosolar
